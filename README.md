6a532b7bea80be97e5225255788b4bd374d7f384
xjh22222228 <xjh22222228@gmail.com>
2020-06-01 20:29:02 +0800

    release

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# use-keep-state
+> useKeppState for React.js
+
+## useKeppState
+Similar to Vue keep-alive, But it is not a component.
+
+
+## Example
+[codesandbox](https://codesandbox.io/s/intelligent-beaver-qofwd?fontsize=14&hidenavigation=1&theme=dark)
+
+
+
+## Install
+```bash
+npm i use-keep-state -S
+```
+
+## Usage
+```js
+import useKeepState from 'use-keep-state';
+
+const namespace = Symbol('App');
+
+export const App = () => {
+  const [state, setState] = useKeepState({
+    name: 'name'
+  }, namespace);
+
+  const onClick = () => {
+    setState({ name: 'Button' });
+  };
+
+  return (
+    <div>
+      <h1>{state.name}</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
+  )
+}
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,79 @@
+import { useReducer, useEffect } from 'react';
+import merge from 'lodash.merge';
+
+let namespace;
+let key;
+const cache = Object.create(null);
+
+function reducer(prevState, nextState) {
+  const v = merge({}, prevState, nextState);
+  cache[namespace] = v;
+  return v;
+}
+
+window.addEventListener('beforeunload', () => {
+  if (!namespace) return;
+  window.sessionStorage.setItem(
+    key,
+    JSON.stringify(cache[namespace])
+  );
+});
+
+function useKeepState(initState, options) {
+  const [state, setState] = useReducer(reducer, initState);
+
+  useEffect(() => {
+    if (Object.prototype.toString.call(options) === '[object Object]') {
+      options = {
+        keepAlive: true,
+        sessionStorage: false,
+        ...options
+      };
+    } else {
+      options = {
+        keepAlive: true,
+        sessionStorage: false,
+        namespace: options
+      };
+    }
+
+    namespace = options.namespace;
+    key = String(namespace);
+
+    if (options.keepAlive) {
+      let v = null;
+      if (options.sessionStorage) {
+        try {
+          v = JSON.parse(window.sessionStorage.getItem(key));
+        } catch {
+          v = cache[namespace];
+        }
+      }
+
+      v && setState(v);
+    }
+
+    return () => {
+      if (options.sessionStorage) {
+        window.sessionStorage.setItem(
+          key,
+          JSON.stringify(cache[namespace])
+        );
+      }
+
+      if (!options.keepAlive) {
+        cache[namespace] = Object.create(null);
+      }
+
+      namespace = null;
+      key = null;
+    };
+  }, []);
+
+  return [
+    state,
+    setState
+  ];
+}
+
+export default useKeepState;
